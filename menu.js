6607c0b13fe4b66364d313f85f856022dad6dfba
Brunno <brunnopereira090@gmail.com>
2023-05-08 10:14:07 -0300

    Subindo pro Hub

---
+++ menu.js
@@ -0,0 +1,9 @@
+let  btn_btn = document.querySelector('.btn_btn')
+console.log(btn_btn)
+
+let navbar_ativo = document.querySelector('.navbar_ativo')
+console.log(navbar_ativo)
+
+btn_btn.addEventListener('click', ()=>{
+    navbar_ativo.classList.toggle('navbar_ativo')
+})
